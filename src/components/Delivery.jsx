d59bd0d584d8793381a4e2b8fb057ccff6da3413
Praw47 <145804997+Praw47@users.noreply.github.com>
2026-02-19 09:50:38 +0530

    Remove commented-out value from input field

--- src/components/Delivery.jsx
+++ src/components/Delivery.jsx
@@ -148,7 +148,6 @@ const handleDownload = async () => {
              placeholder='Name' />
             <span >Total:</span>
             <input type="number"
-              {/* value={getAmountShip} */}
               style={{marginRight:"100px"
               }}  placeholder='Total' />
             <input type="text" className='empty' disabled />
